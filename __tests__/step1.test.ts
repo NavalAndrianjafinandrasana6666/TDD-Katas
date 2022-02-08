48ce1678e136e20ad2976fa97e42cb26d5b12cfb
NavaL <ayoyo@thoughtworks.com>
2022-02-08 15:09:15 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- __tests__/step1.test.ts
+++ __tests__/step1.test.ts
@@ -5,3 +5,16 @@ Distance is rounded up to the nearest 0.5km, and waiting time is rounded up to t
 Total Fare = $2 per kilometer + $1 per minute of waiting
 
 * */
+
+import { calculateFare } from "../src/fare";
+
+describe("Ride", () => {
+    it("should return fare given distance and waiting time", () => {
+        const distanceInKilometer = 1;
+        const waitingTimeInMinutes = 2;
+        const fare = calculateFare(distanceInKilometer, waitingTimeInMinutes);
+  
+      expect(fare).toEqual(4);
+    });
+  });
+
